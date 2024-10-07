ebd1d800de4036a8ef3abdde96b6700bd641b5eb
Stuart Maxwell <stuart@amanzi.nz>
2024-10-07 21:35:49 +1300

    Update comments in file

--- src/djpress/apps.py
+++ src/djpress/apps.py
@@ -11,5 +11,6 @@ class DjpressConfig(AppConfig):
     label = "djpress"
 
     def ready(self: "DjpressConfig") -> None:
-        """Import signals."""
+        """Run when the app is ready."""
+        # Import signals to ensure they are registered
         import djpress.signals  # noqa: F401
