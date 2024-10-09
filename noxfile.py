a3fbdebb6737022b6734289f0396f23cf5577fb7
Stuart Maxwell <stuart@amanzi.nz>
2024-10-10 01:06:40 +1300

    Fix noxfile to install as editable

--- noxfile.py
+++ noxfile.py
@@ -7,6 +7,6 @@ import nox
 @nox.parametrize("django_ver", ["~=4.2.0", "~=5.0.0", "~=5.1.0"])
 def test(session: nox.Session, django_ver: str) -> None:
     """Run the test suite."""
-    session.install(".", "--all-extras", "-r", "pyproject.toml")
+    session.install("-e", ".", "--extra", "test", "-r", "pyproject.toml")
     session.install(f"django{django_ver}")
     session.run("pytest")
