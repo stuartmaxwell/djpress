912a9eff871563a0a7b1c57500729b5cdc4001e0
Stuart Maxwell <stuart@amanzi.nz>
2024-10-09 23:27:39 +1300

    Update admin to better display models

--- src/djpress/admin.py
+++ src/djpress/admin.py
@@ -1,9 +1,25 @@
 """djpress admin configuration."""
 
+from typing import ClassVar
+
 from django.contrib import admin
 
 # Register the models here.
 from djpress.models import Category, Post
 
-admin.site.register(Category)
-admin.site.register(Post)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin configuration."""
+
+    list_display: ClassVar["str"] = ["title", "slug"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin configuration."""
+
+    list_display: ClassVar["str"] = ["post_type", "title", "slug", "date", "author"]
+    list_display_links: ClassVar["str"] = ["title", "slug"]
+    ordering: ClassVar["str"] = ["post_type", "-date"]  # Displays pages first, then sorted by date.
+    list_filter: ClassVar["str"] = ["post_type", "date", "author"]
