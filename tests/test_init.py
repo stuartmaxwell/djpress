88e03bc6705bf33d52ee9f546d0237d728c07de9
Stuart Maxwell <stuart@amanzi.nz>
2024-11-13 23:03:14 +1300

    Update test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,34 +1,8 @@
 import importlib
+import sys
 from unittest.mock import mock_open, patch
 
 import pytest
-
-
-def test_version_loading():
-    mock_toml_content = """
-    [project]
-    version = "1.0.0"
-    """
-
-    with patch("pathlib.Path.open", mock_open(read_data=mock_toml_content)):
-        with patch("tomllib.load") as mock_load:
-            mock_load.return_value = {"project": {"version": "1.0.0"}}
-
-            import djpress
-
-            importlib.reload(djpress)  # Reload the module to apply the mock
-
-            assert djpress.__version__ == "1.0.0"
-
-
-def test_version_loading_error():
-    with patch("pathlib.Path.open", mock_open()) as mock_file:
-        mock_file.side_effect = FileNotFoundError()
-
-        with pytest.raises(FileNotFoundError):
-            import djpress
-
-            importlib.reload(djpress)  # Reload the module to apply the mock
 
 
 def test_tomllib_import_error():
@@ -39,11 +13,16 @@ def test_tomllib_import_error():
 
     with patch.dict("sys.modules", {"tomllib": None}):
         with patch("pathlib.Path.open", mock_open(read_data=mock_toml_content)):
-            with patch("tomli.load") as mock_load:
-                mock_load.return_value = {"project": {"version": "1.0.0"}}
+            if sys.version_info < (3, 11):
+                # tomli only installed for Python 3.10 and below
+                with patch("tomli.load") as mock_load:
+                    mock_load.return_value = {"project": {"version": "1.0.0"}}
+                    import djpress
 
-                import djpress
+                    importlib.reload(djpress)  # Reload the module to apply the mock
+                    assert djpress.__version__ == "1.0.0"
+            else:
+                with pytest.raises((ImportError, TypeError)):  # TypeError for VS Code?
+                    import djpress
 
-                importlib.reload(djpress)  # Reload the module to apply the mock
-
-                assert djpress.__version__ == "1.0.0"
+                    importlib.reload(djpress)
