ade864b235cbc71a5834b5d29ad471bfa01062e2
Stuart Maxwell <stuart@amanzi.nz>
2024-11-20 00:47:46 +1300

    Add Django check

---
+++ src/djpress/checks.py
@@ -0,0 +1,23 @@
+"""Custom checks for DJPress."""
+
+from django.core.checks import Warning, register
+
+
+@register()
+def check_plugin_hooks(app_configs, **kwargs) -> list[Warning]:  # noqa: ANN001, ANN003, ARG001
+    """Check for unknown plugin hooks."""
+    from djpress.plugins import Hooks, registry
+
+    warnings = []
+
+    for hook_name in registry.hooks:
+        if not isinstance(hook_name, Hooks):
+            warning = Warning(
+                f"Plugin registering unknown hook '{hook_name}'.",
+                hint=("This might indicate use of a deprecated hook or a hook from a newer version of DJPress."),
+                obj=hook_name,
+                id="djpress.W001",
+            )
+            warnings.append(warning)
+
+    return warnings
