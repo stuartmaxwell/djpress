daa3f0b492995bd43d1c0349996b4c1892c8de07
Stuart Maxwell <stuart@amanzi.nz>
2024-11-26 21:28:00 +1300

    Improve the admin layout

--- src/djpress/admin.py
+++ src/djpress/admin.py
@@ -3,6 +3,8 @@
 from typing import ClassVar
 
 from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Register the models here.
 from djpress.models import Category, PluginStorage, Post
@@ -19,10 +21,72 @@ class CategoryAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     """Post admin configuration."""
 
-    list_display: ClassVar["str"] = ["post_type", "title", "slug", "date", "author"]
-    list_display_links: ClassVar["str"] = ["title", "slug"]
-    ordering: ClassVar["str"] = ["post_type", "-date"]  # Displays pages first, then sorted by date.
-    list_filter: ClassVar["str"] = ["post_type", "date", "author"]
+    list_display: ClassVar[list[str]] = ["post_type", "published_status", "title", "parent", "formatted_date", "author"]
+    list_display_links: ClassVar[list[str]] = ["title"]
+    ordering: ClassVar[list[str]] = ["post_type", "-date"]  # Displays pages first, then sorted by date.
+    list_filter: ClassVar[list[str]] = ["post_type", "date", "author"]
+    prepopulated_fields: ClassVar[dict[str, str]] = {"slug": ("title",)}
+    search_fields: ClassVar[list[str]] = ["title", "content", "slug"]
+    readonly_fields: ClassVar[list[str]] = ["modified_date"]
+    fieldsets: ClassVar = [
+        (
+            None,
+            {
+                "fields": ("title", "slug", "author"),
+            },
+        ),
+        (
+            "Content",
+            {
+                "fields": ("content",),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Publishing",
+            {
+                "fields": (
+                    "status",
+                    "post_type",
+                    "date",
+                    "modified_date",
+                ),
+            },
+        ),
+        (
+            "Post Settings",
+            {
+                "fields": ("categories",),
+                "description": "These settings only apply to posts",
+            },
+        ),
+        (
+            "Page Settings",
+            {
+                "fields": ("parent", "menu_order"),
+                "description": "These settings only apply to pages",
+            },
+        ),
+    ]
+
+    @admin.display(
+        boolean=True,
+        description="Published",
+    )
+    def published_status(self, obj: Post) -> bool:
+        """Display published status using Django's built-in boolean icon."""
+        return obj.is_published
+
+    def formatted_date(self, obj: Post) -> str:
+        """Format the date and show future posts distinctly."""
+        if obj.date > timezone.now():
+            return format_html(
+                '<span style="color: #666;">{} (Scheduled)</span>',
+                obj.date.strftime("%Y-%m-%d %H:%M"),
+            )
+        return obj.date.strftime("%Y-%m-%d %H:%M")
+
+    formatted_date.short_description = "Date"
 
 
 @admin.register(PluginStorage)
