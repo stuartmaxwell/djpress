deb8137886a760a1ab558b1f84af638b34f8faa0
Stuart Maxwell <stuart@amanzi.nz>
2024-12-16 22:47:41 +1300

    Add test for category permissions

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -36,3 +36,19 @@ def test_group_permissions(post_content_type: ContentType) -> None:
     assert publish_perm in editor.permissions.all()
     assert publish_perm in author.permissions.all()
     assert publish_perm not in contributor.permissions.all()
+
+
+@pytest.mark.django_db
+def test_grou_category_permissions(post_content_type: ContentType) -> None:
+    editor = Group.objects.get(name="editor")
+    author = Group.objects.get(name="author")
+    contributor = Group.objects.get(name="contributor")
+
+    # Check category permissions
+    category_perms = Permission.objects.filter(
+        content_type=post_content_type, codename__in=["add_category", "change_category", "delete_category"]
+    )
+    for perm in category_perms:
+        assert perm in editor.permissions.all()
+        assert perm not in author.permissions.all()
+        assert perm not in contributor.permissions.all()
