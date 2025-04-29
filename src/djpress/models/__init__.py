d05baff91851f9a60986d58dde6d874963883133
Stuart Maxwell <stuart@amanzi.nz>
2025-04-29 22:29:48 +1200

    Add Media model and migration for handling uploaded media files

--- src/djpress/models/__init__.py
+++ src/djpress/models/__init__.py
@@ -1,8 +1,9 @@
 """Models package for djpress app."""
 
 from djpress.models.category import Category
+from djpress.models.media import Media
 from djpress.models.plugin_storage import PluginStorage
 from djpress.models.post import Post
 from djpress.models.tag import Tag
 
-__all__ = ["Category", "PluginStorage", "Post", "Tag"]
+__all__ = ["Category", "Media", "PluginStorage", "Post", "Tag"]
