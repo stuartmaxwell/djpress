a5545167113c8517ed86f5819eb279adc68350a9
Stuart Maxwell <stuart@amanzi.nz>
2025-11-04 21:43:39 +1300

    Moved the groups and permissions out of signals

--- src/djpress/signals.py
+++ src/djpress/signals.py
@@ -1,13 +1,10 @@
 """Signals for djpress app."""
 
 from django.apps import AppConfig
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 from django.db.models.signals import post_delete, post_migrate, post_save
 from django.dispatch import receiver
 
-from djpress.models import Media
 from djpress.models.category import (
     CATEGORY_CACHE_KEY,
     Category,
@@ -20,59 +17,16 @@ from djpress.models.tag import (
     TAG_CACHE_KEY,
     Tag,
 )
+from djpress.permissions import create_groups as setup_groups
 
 
 @receiver(post_migrate)
 def create_groups(sender: AppConfig, **_) -> None:  # noqa: ANN003
-    """Create groups and assign permissions."""
+    """Create groups and assign permissions after migrations."""
     if sender.name != "djpress":
         return
 
-    # Get content types
-    post_content_type = ContentType.objects.get_for_model(Post)
-    category_content_type = ContentType.objects.get_for_model(Category)
-
-    # Get permissions
-    standard_permissions = Permission.objects.filter(
-        content_type=post_content_type,
-        codename__in=["add_post", "change_post", "delete_post"],
-    )
-    publish_permission = Permission.objects.get(
-        content_type=post_content_type,
-        codename="can_publish_post",
-    )
-    category_permissions = Permission.objects.filter(
-        content_type=category_content_type,
-        codename__in=["add_category", "change_category", "delete_category"],
-    )
-    tag_permissions = Permission.objects.filter(
-        content_type=ContentType.objects.get_for_model(Tag),
-        codename__in=["add_tag", "change_tag", "delete_tag"],
-    )
-    tag_add_permission = Permission.objects.get(
-        content_type=ContentType.objects.get_for_model(Tag),
-        codename="add_tag",
-    )
-    media_permissions = Permission.objects.filter(
-        content_type=ContentType.objects.get_for_model(Media),
-        codename__in=["add_media", "change_media", "delete_media"],
-    )
-
-    # Create groups and assign permissions
-    editor_group, _ = Group.objects.get_or_create(name="editor")
-    editor_group.permissions.add(
-        publish_permission,
-        *standard_permissions,
-        *category_permissions,
-        *tag_permissions,
-        *media_permissions,
-    )
-
-    author_group, _ = Group.objects.get_or_create(name="author")
-    author_group.permissions.add(publish_permission, *standard_permissions, tag_add_permission, *media_permissions)
-
-    contributor_group, _ = Group.objects.get_or_create(name="contributor")
-    contributor_group.permissions.add(*standard_permissions, tag_add_permission, *media_permissions)
+    setup_groups()
 
 
 @receiver(post_save, sender=Category)
